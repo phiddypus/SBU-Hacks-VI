932c05d7cd23c8f86d9bbf61fe353650cfe9363b
phiddypus <phiddypus@proton.me>
2024-02-09 23:59:43 -0500

    player and cam

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENS = 0.003
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,7 +22,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized().rotated(Vector3.UP, rotation.y)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -29,3 +31,13 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		var y_rotation = event.relative.x * MOUSE_SENS
+		rotation.y -= y_rotation
+		$Camera3D.rotation.y -= y_rotation
+		$Camera3D.rotation.x -= event.relative.y * MOUSE_SENS
+
+func _process(delta):
+	pass
