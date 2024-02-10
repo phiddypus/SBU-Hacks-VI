bfd0e234fe5a63210b4ac70362c6fec9eae82f55
phiddypus <phiddypus@proton.me>
2024-02-10 11:00:48 -0500

    jump pad

--- godot/scripts/jump_pad.gd
+++ godot/scripts/jump_pad.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+@export var jump_pad_strength = 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,3 +10,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	if body is CharacterBody3D:
+		body.velocity.y = jump_pad_strength
