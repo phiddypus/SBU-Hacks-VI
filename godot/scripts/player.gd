00cb937e7efe759c21ae2ab6c5fb5d60970e44d9
phiddypus <phiddypus@proton.me>
2024-02-10 12:05:17 -0500

    fix move

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -3,7 +3,6 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENS = 0.003
-var target_velocity = Vector3(0,0,0)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -13,24 +12,15 @@ func _ready():
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	else:
 		velocity.y -= gravity * delta
-
-	if(is_on_floor()):
-		target_velocity = Vector3(0,0,0)
 	
 	# Handle jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		
-		# If Jumping on Moving Platform
-		var collision = move_and_collide(Vector3(0,-1,0)*delta)
-		if collision:
-			if collision.get_collider().get_class() == "AnimatableBody3D":
-				velocity += collision.get_collider().velocity
-				target_velocity = collision.get_collider().velocity
 		velocity.y = JUMP_VELOCITY
-		
-		
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("game_left", "game_right", "game_front", "game_back")
@@ -38,12 +28,8 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, target_velocity.x, SPEED)
-		velocity.z = move_toward(velocity.z, target_velocity.z, SPEED)
 	
 	move_and_slide()
-		
 
 func _input(event):
 	if event is InputEventMouseMotion:
