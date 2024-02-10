6b6bf009aa29edc77c3b8dbcbaa42d2f1a345b98
phiddypus <phiddypus@proton.me>
2024-02-10 17:14:40 -0500

    rudimentary level scene

--- godot/scripts/level.gd
+++ godot/scripts/level.gd
@@ -4,13 +4,20 @@ extends Node3D
 @export var endPos = Vector3.ZERO
 
 var started = false
+var stopped = false
 var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	endPos = $End.position
+	started = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if started:
+	if started and not stopped:
 		time += delta
+
+func _on_end_body_entered(body):
+	if body is CharacterBody3D:
+		stopped = true
+		print(time)
