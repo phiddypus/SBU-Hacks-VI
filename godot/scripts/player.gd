4c071d8485a53c6b6da720113d874058dfaf10ff
phiddypus <phiddypus@proton.me>
2024-02-10 15:52:07 -0500

    ok now i fixed

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -1,37 +1,55 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+const WALK_SPEED = 5.0
+const DASH_SPEED = WALK_SPEED ** 2
+const WALK_DAMP = 0.25
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENS = 0.003
 const DASH_COOLDOWN = 1.5
+
+var base_velocity = Vector3.ZERO
 var dash_timer = 0
 var dashing = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func get_ground_velocity ():
+	for i in range(get_slide_collision_count()):
+		var coll = get_slide_collision(i)
+		if coll.get_angle() == get_floor_angle():
+			var collColl = coll.get_collider()
+			if "velocity" in collColl:
+				return collColl.velocity
+	return Vector3.ZERO
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
-	# Add the gravity.
-	if is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	else:
-		velocity.y -= gravity * delta
-	
-	# Handle jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var on_floor = is_on_floor()
+	var velXZ = Vector3(velocity.x, 0, velocity.z)
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("game_left", "game_right", "game_front", "game_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized().rotated(Vector3.UP, rotation.y)
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-			
+	var move = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized().rotated(Vector3.UP, rotation.y) * WALK_SPEED
+	var target_velocity = Vector3.ZERO
+	if move:
+		target_velocity = move
+	if not on_floor:
+		target_velocity = (target_velocity + velXZ).limit_length(max(velXZ.length(), WALK_SPEED))
+		#print(target_velocity)
+
+	velXZ = velXZ.lerp(target_velocity, WALK_DAMP)
+	velocity.x = velXZ.x
+	velocity.z = velXZ.z
+
+	if on_floor:
+		base_velocity = get_ground_velocity()
+		if Input.is_action_just_pressed("game_jump"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		velocity.y -= gravity * delta
 	
 	# Dash
 	if dash_timer <= DASH_COOLDOWN:
@@ -39,12 +57,12 @@ func _physics_process(delta):
 		
 	if dashing:
 		var camera = $Camera3D
-		var camera_dir = $Camera3D.basis.z
+		#var camera_dir = $Camera3D.basis.z
 		var dir = Basis(Vector3(1,0,0),camera.rotation.x*2)*Basis(Vector3(0,1,0), camera.rotation.y*2)*Vector3(0,0,1)
-		print(camera.basis.z.y)
-		velocity.x = -dir.x * SPEED * SPEED
-		velocity.y = -camera.basis.z.y * SPEED * SPEED
-		velocity.z = -dir.z * SPEED * SPEED
+		#print(camera.basis.z.y)
+		velocity.x = -dir.x * DASH_SPEED
+		velocity.y = -camera.basis.z.y * DASH_SPEED
+		velocity.z = -dir.z * DASH_SPEED
 		dash_timer=0
 		dashing=false
 	
@@ -63,5 +81,5 @@ func _input(event):
 			dashing=true
 			
 
-func _process(delta):
-	pass
+#func _process(delta):
+	#pass
