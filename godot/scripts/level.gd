7afe2720770667ed4a03b481a7662271a3c3bdbf
phiddypus <phiddypus@proton.me>
2024-04-12 23:13:57 -0400

    pre export fixes

--- godot/scripts/level.gd
+++ godot/scripts/level.gd
@@ -2,33 +2,34 @@ extends Node3D
 
 var startPos = Vector3.ZERO
 var endPos = Vector3.ZERO
-enum cplt_statuses {
+enum COMPLETION {
 	NOT_STARTED,
 	IN_PROGRESS,
 	FINISHED,
 }
-var completion = cplt_statuses.NOT_STARTED
+var completion = COMPLETION.NOT_STARTED
 var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	startPos = $Player.position
 	endPos = $End.position
-	completion = cplt_statuses.NOT_STARTED
-	completion = cplt_statuses.IN_PROGRESS
+	completion = COMPLETION.NOT_STARTED
+	completion = COMPLETION.IN_PROGRESS
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if completion == cplt_statuses.IN_PROGRESS:
+	if completion == COMPLETION.IN_PROGRESS:
 		time += delta
 
 func _on_end_body_entered(body):
 	if body is CharacterBody3D:
-		completion = cplt_statuses.FINISHED
+		completion = COMPLETION.FINISHED
 		print("Your time: " + str(time))
 
 func _input(event):
 	if event.is_action("game_reset"):
 		time = 0
+		completion = COMPLETION.IN_PROGRESS
 		$Player.position = startPos
 		$Player.velocity = Vector3.ZERO
